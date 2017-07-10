2131fcef985fbd4dff153aa423dd0841fa26143a
Ryan Peters <petersryan84@gmail.com>
2017-07-10 01:16:17 -0700

    Update flip.py

--- flip.py
+++ flip.py
@@ -1,16 +1,28 @@
 import random
-print('Type roll when ready.')
 
-roll = random.randint(0, 1)
+
+# Function deffinitions
+
 def munch():
+    roll = random.randint(0, 1)
     if roll == 1:
         print('you win')
-        print('would you like to play again?')
-        if input()== 'yes':
-            return
-    if roll == 0:
+    else:
+        print('tough cookies stool pusher, you rolled a zero')
+    print('would you like to play again?')
+    getInput('yes')
 
-if input() = 'roll':
-    munch()
-    else print ('what?'):
-        return
+    
+def getInput(expected):
+    if input() == expected:
+        munch()
+    else:
+        print('Thank you for playing flip.py, see you next time!')
+
+
+
+# Scripting code
+
+
+print('Type roll when ready.')
+getInput('roll')
