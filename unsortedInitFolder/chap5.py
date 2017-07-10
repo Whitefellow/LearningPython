03151b32da791d8787aa953da59b5f65d7b308a7
Ryan Peters <petersryan84@gmail.com>
2017-07-09 23:31:52 -0700

    Update chap5.py

--- unsortedInitFolder/chap5.py
+++ unsortedInitFolder/chap5.py
@@ -1,6 +1,13 @@
 spam = ['apples', 'bananas', 'tofu', 'cats']
+
+# show the contents of the list
+print(spam)
+
+# now ask the user for instructions for what they want done
 print('Enter an action')
-input()
+input() # input pauses the program until the user presses enter.
+
+#
 if input == 'add':
-    spam[] = spam + none # none might need some explaining
+    spam[] = spam + none # This is just a case example of iterating through a list and having nothing happen to that list.
 
